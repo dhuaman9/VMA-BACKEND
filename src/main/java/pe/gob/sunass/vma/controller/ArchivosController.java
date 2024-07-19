3ff9375aa4978cb0e03e99809725a1094fd6d9f2
dhuaman <dhuaman@sunass.gob.pe>
2024-07-19 16:35:24 -0500

    cambios en archivoController

--- src/main/java/pe/gob/sunass/vma/controller/ArchivosController.java
+++ src/main/java/pe/gob/sunass/vma/controller/ArchivosController.java
@@ -11,39 +11,34 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import pe.gob.sunass.vma.dto.ArchivoDTO;
+import pe.gob.sunass.vma.dto.UsuarioDTO;
+import pe.gob.sunass.vma.exception.ConflictException;
 import pe.gob.sunass.vma.exception.FailledValidationException;
+import pe.gob.sunass.vma.exception.ResourceNotFoundException;
 import pe.gob.sunass.vma.service.AlfrescoService;
 
 @RestController
-@RequestMapping("/api/files")
+@RequestMapping("/archivo")
 public class ArchivosController {
 
 	
-	 private static Logger logger = LoggerFactory.getLogger(ArchivosController.class);
+	private static Logger logger = LoggerFactory.getLogger(ArchivosController.class);
 	 
-		@Autowired
-	    private AlfrescoService alfrescoService;
+	@Autowired
+	private AlfrescoService alfrescoService;
 		
-		
-
-	    /*@PostMapping("/upload")
-	    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
-	        try {
-	            String response = alfrescoService.uploadFile(file);
-	            return ResponseEntity.ok(response);
-	        } catch (IOException e) {
-	            return ResponseEntity.status(500).body("Failed to upload file: " + e.getMessage());
-	        }
-	    }*/
+	  
 
 	@PostMapping("/upload")
 	public ResponseEntity<List<ArchivoDTO>> uploadFiles(@RequestParam(value = "pdf", required = false) MultipartFile pdf,
@@ -73,54 +68,46 @@ public class ArchivosController {
 		}
 	}
 	    
-		/* @PostMapping("/upload/pdf")
-		    public ResponseEntity<ArchivoDTO> uploadPdfFile(@RequestParam("file") MultipartFile file) {
-		        try {
-		            ArchivoDTO archivoDTO = alfrescoService.uploadFile(file);
-		            return new ResponseEntity<>(archivoDTO, HttpStatus.OK);
-		            
-		        } catch (FailledValidationException ex) {  //BadRequestException ex
-			    	
-			    	throw ex;
-			    } catch (Exception ex) {
-				      logger.error(ex.getMessage(), ex);
-				       return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-				 }
-		    }
-
-		    @PostMapping("/upload/pdf-word")
-		    public ResponseEntity<ArchivoDTO> uploadPDFWordFile(@RequestParam("file") MultipartFile file) {
-		        try {
-		            ArchivoDTO archivoDTO = alfrescoService.uploadFile(file);
-		            return new ResponseEntity<>(archivoDTO, HttpStatus.OK);
-		        } catch (FailledValidationException ex) {  //BadRequestException ex
-			    	
-			    	throw ex;
-			    } catch (Exception ex) {
-				      logger.error(ex.getMessage(), ex);
-				       return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-				 }
-		    }
-
-		    @PostMapping("/upload/excel")
-		    public ResponseEntity<ArchivoDTO> uploadExcelFile(@RequestParam("file") MultipartFile file) {
-		        try {
-		            ArchivoDTO archivoDTO = alfrescoService.uploadFile(file);
-		            return new ResponseEntity<>(archivoDTO, HttpStatus.OK);
-		        } catch (FailledValidationException ex) {  //BadRequestException ex
-			    	
-			    	throw ex;
-			    } catch (Exception ex) {
-				      logger.error(ex.getMessage(), ex);
-				       return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-				 }
-		    }*/
-	   
-	//descarga de ficheros
+	@PutMapping("/{id}")
+    public ResponseEntity<ArchivoDTO> updateFile(@PathVariable("id") Integer archivoId,
+                                                  @RequestParam("file") MultipartFile file) {
+        try {
+            // Llamar al servicio para actualizar el archivo
+            ArchivoDTO updatedArchivoDTO = alfrescoService.updateFile(archivoId, file);
+            // Devolver una respuesta con el archivo actualizado
+            
+           //return ResponseEntity.ok(updatedArchivoDTO);
+            return new ResponseEntity<ArchivoDTO>(updatedArchivoDTO,
+                    HttpStatus.ACCEPTED);
+        } catch (ConflictException e) {
+            throw e; // Let the GlobalExceptionHandler handle it
+        }catch (IOException e) {
+            
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 	
-	 /*@GetMapping("/{nodeId}/download")
-	    public ResponseEntity<byte[]> downloadFile(@PathVariable String nodeId) {
+//	@PutMapping("/{id}")
+//    public ResponseEntity<ArchivoDTO> updateFile2(@PathVariable Integer id,
+//            @RequestParam("file") MultipartFile file) {
+//        try {
+//            ArchivoDTO updatedArchivo = alfrescoService.updateFile(id, file);
+//            return ResponseEntity.ok(updatedArchivo);
+//        } catch (RuntimeException | IOException e) {
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+//        }
+//    }
+//  
+	 @GetMapping("/{nodeId}/download")
+	  public ResponseEntity<byte[]> downloadFile(@PathVariable String nodeId) {
 	        return alfrescoService.downloadFile(nodeId);
-	    }*/
+	  }
+	 
+	  
+	  
+	 @DeleteMapping("/{nodeId}")
+	  public ResponseEntity<Void> deleteFile(@PathVariable String nodeId) {
+	        return alfrescoService.deleteFile(nodeId);
+	 }
 	    
 }
